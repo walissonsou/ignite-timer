7f0041245fd90fce1c1fe5a983b3bc1607e0ea51
walissonsou <walissonsouza96@hotmail.com>
2022-11-20 21:27:07 -0300

    populando meu componente countdown para depois usar contexto

--- src/pages/Home/components/CountDown/index.tsx
+++ src/pages/Home/components/CountDown/index.tsx
@@ -1,56 +1,23 @@
-import { useEffect } from "react";
+import { useContext, useEffect } from "react";
+import { CyclesContext } from "../../Home";
 import { CountdownContainer, Separator } from "./styles";
 
 export function CountDown() {
-  return (
-    <>
-    
-    
-    
-    const activeCycle = cycle.find(cycle => cycle.id == activeId)
-    // calculo o segundo que foi passado no input
-    const totalDeSegundos = activeCycle ? activeCycle.timer * 60 : 0
+  const totalDeSegundos = activeCycle ? activeCycle.timer * 60 : 0
+  const [qQtddDeSegundosPassados, setQtddSegundosPassados] = useState(0)
 
-    useEffect(() => {
-      let interval: number
-  
-      if (activeCycle) {
-        interval = setInterval(() => {
-          const secondsDiference = differenceInSeconds(
-            new Date(),
-            activeCycle.start
-          )
-  
-          if (secondsDiference >= totalDeSegundos) {
-            setCycle(
-              cycle.map((cycle) => {
-                if (cycle.id == activeId) {
-                  return { ...cycle, finishDate: new Date() }
-                } else
-                  return cycle
-              })
-            )
-          } else {
-            setQtddSegundosPassados(secondsDiference)
-          }
-         
-        }, 1000)
-      }
-  
-      return () => {
-        clearInterval(interval)
-      }
-    }, [activeCycle, totalDeSegundos])
-  
+  const minutes = String(quantidadeDeMinutos).padStart(2, '0')
+  const seconds = String(quantidadeDeSegundos).padStart(2, '0')
+
+  return (   
 
 
-    <CountdownContainer>
+  <CountdownContainer>
     <span>{minutes[0]}</span>
     <span>{minutes[1]}</span>
     <Separator>:</Separator>
     <span>{seconds[0]}</span>
     <span>{seconds[1]}</span>
   </CountdownContainer>
-  </>
   )
 }
